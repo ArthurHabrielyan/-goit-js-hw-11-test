567f2a87e06adc9d7c02171300b2c66c5b7185f6
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-03-29 15:51:27 +0300

    api-service

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-import '../src/css/styles.css';
-import ImageApiSrvice from './js/api-service';
-import imgCardTpl from './tamplate/img-card.hbs';
-import LoadMoreBtn from './js/components/load-more-btn'
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import getRefs from './js/get-refs';
+import "../src/css/styles.css";
+import ImageApiSrvice from "./js/api-service";
+import imgCardTpl from "./tamplate/img-card.hbs";
+import LoadMoreBtn from "./js/components/load-more-btn";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+import getRefs from "./js/get-refs";
 
 const imageApiService = new ImageApiSrvice();
 const loadMoreBtn = new LoadMoreBtn({
-  selector: '.load-more',
-  hiddden: true
-})
+  selector: ".load-more",
+  hidden: true,
+});
 
 const refs = getRefs();
 
-refs.searchFormEl.addEventListener('submit', onSearchFormSubmit);
-loadMoreBtn.refs.button.addEventListener('click', onFatchData);
-
+refs.searchFormEl.addEventListener("submit", onSearchFormSubmit);
+loadMoreBtn.refs.button.addEventListener("click", onFatchData);
 
 async function onSearchFormSubmit(event) {
   event.preventDefault();
-
   imageApiService.input = event.currentTarget.elements.searchQuery.value.trim();
   event.currentTarget.reset();
   if (!imageApiService.input) {
     clearGalleryContainer();
-    return Notify.failure("Please enter a valid string!")
+    return Notify.failure("Please enter a valid string!");
   }
 
   loadMoreBtn.show();
@@ -34,7 +32,9 @@ async function onSearchFormSubmit(event) {
   if (dataAcquisition.hits.length === 0) {
     imageApiService.resetPage();
     clearGalleryContainer();
-    return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    return Notify.failure(
+      "Sorry, there are no images matching your search query. Please try again."
+    );
   } else {
     imageApiService.resetPage();
     clearGalleryContainer();
@@ -44,43 +44,40 @@ async function onSearchFormSubmit(event) {
 }
 
 async function onFatchData(data) {
-  loadMoreBtn.disable()
-  imageApiService.decrementPage()
+  loadMoreBtn.disable();
+  imageApiService.decrementPage();
 
-  const additionalData = await imageApiService.fetchImageFromDb()
+  const additionalData = await imageApiService.fetchImageFromDb();
   renderImgCard(additionalData);
-  loadMoreBtn.enable()
-  if(additionalData.hits.length === 0 || !imageApiService.input) {
+  loadMoreBtn.enable();
+  if (additionalData.hits.length === 0 || !imageApiService.input) {
     loadMoreBtn.hide();
     imageApiService.resetPage();
-    return Notify.failure("We're sorry, but you've reached the end of search results.")
+    return Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
   }
   imageApiService.resetPage();
 }
 
 function renderImgCard(data) {
-  refs.gallery.insertAdjacentHTML('beforeend', imgCardTpl(data))
+  refs.gallery.insertAdjacentHTML("beforeend", imgCardTpl(data));
 }
 
 function clearGalleryContainer() {
-  refs.gallery.innerHTML = '';
+  refs.gallery.innerHTML = "";
 }
 
+async function fetchImageFromDb() {
+  const url = `${BASE_URL}?key=${KEY}&q=${this.inputValue}&lang=en&per_page=40&page=${this.page}&
+    image_type="photo&orientation=horizontal&safesearch=true"`;
 
+  try {
+    const response = await axios.get(url);
+    const data = await response.data;
 
-
-// const fn = async function() {
-
-// }
-
-// const arr = async () => {}
-
-// const x = {
-//   async getName () {}
-// }
-
-// class X {
-//   async getName () {
-
-//   }
-// }
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/js/components/load-more-btn.js
+++ src/js/components/load-more-btn.js
@@ -1,40 +1,39 @@
 export default class LoadMoreBtn {
-    constructor({selector, hiddden = false}) {
-        this.refs = this.getRefs(selector);
+  constructor({ selector, hidden = false }) {
+    this.refs = this.getRefs(selector);
 
-        if(hiddden) {
-            this.hide();
-        }
+    if (hidden) {
+      this.hide();
     }
+  }
 
-
-getRefs(selector) {
+  getRefs(selector) {
     // console.log('ref', selector);
     const refs = {};
     refs.button = document.querySelector(selector);
-    refs.label = refs.button.querySelector('.label');
-    refs.spinner = refs.button.querySelector('.spinner');
+    refs.label = refs.button.querySelector(".label");
+    refs.spinner = refs.button.querySelector(".spinner");
 
     return refs;
-}
+  }
 
-enable() {
+  enable() {
     this.refs.button.disabled = false;
-    this.refs.label.textContent = 'Load more';
-    this.refs.spinner.classList.add('is-hidden');
-}
+    this.refs.label.textContent = "Load more";
+    this.refs.spinner.classList.add("is-hidden");
+  }
 
-disable() {
+  disable() {
     this.refs.button.disabled = true;
-    this.refs.label.textContent = 'Loading...';
-    this.refs.spinner.classList.remove('is-hidden');
-}
+    this.refs.label.textContent = "Loading...";
+    this.refs.spinner.classList.remove("is-hidden");
+  }
 
-show() {
-    this.refs.button.classList.remove('is-hidden');
-}
+  show() {
+    this.refs.button.classList.remove("is-hidden");
+  }
 
-hide() {
-    this.refs.button.classList.add('is-hidden');
-}
+  hide() {
+    this.refs.button.classList.add("is-hidden");
+  }
 }
